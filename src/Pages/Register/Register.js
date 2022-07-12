f728646dc54471bb95377d9b4599d7a1a072dad6
Rizal Lazuardi Firdaus <93332704+rzlzrdf@users.noreply.github.com>
2022-07-12 21:14:08 +0700

    login - regist fitur

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -25,18 +25,29 @@ const Register = () => {
 
 	//membuat submit handler
 	const formSubmitHandler = (data) => {
-		
+		console.log('formSubmitHandler Jalan')
 
-		//membuat template JSON dari input pada form
+		//membuat template rare JSON dari input pada form (form - data)
 		const postData = {
 			nama: data.user_name,
 			email: data.user_email,
 			password: data.user_password
 		}
 
+		//membuat template x-www-form-urlencoded
+		let formData = new FormData();
+		formData.append('nama', data.user_name);
+		formData.append('email', data.user_email,);
+		formData.append('password', data.user_password);
 		console.log(postData)
 
-		axios.post('https://secondhandapp.herokuapp.com/api/auth/register', postData) //post object template dari from ke link API
+		
+		axios({
+			method: "post",
+			url: "https://secondhandapp.herokuapp.com/api/auth/register",
+			data: postData,
+			headers: { "Content-Type": "application/json" },
+		 })//post object template dari from ke link API
 		.then((res) => {
 			if(typeof res.data.acessToken !== 'undefined'){
 				localStorage.setItem('secondHandToken', res.data.acessToken)
@@ -44,7 +55,7 @@ const Register = () => {
 
 			//menyimpan di redux store
 			const user = jwtDecode(res.data.acessToken)
-			axios.get(/*`https://finalsecondhand-staging.herokuapp.com/User/DetailUser${user.sub}`*/)
+			axios.get(`https://secondhandapp.herokuapp.com/api/user/detail-user'${user.sub}`)
 				.then((res) => {
 					dispatch(
 						userSlice.actions.addUser({userData: res.data})
@@ -70,7 +81,7 @@ const Register = () => {
 		<div className={style.register_form}>
 		  <Link to="/"><img src="./Img/logo.svg" alt="" className="py-4" /></Link>
 			<h3 className="fw-bold my-3">Daftar</h3>
-			<form className="form" method="post" onSubmit={handleSubmit(formSubmitHandler)}>
+			<form className="form" id="register-form" method="post" onSubmit={handleSubmit(formSubmitHandler)}>
 				<div className="form-group mt-3">
 					<label htmlFor="name">Nama</label>
 					<input type="text" className={"form-control "+ style.input_} id="name" placeholder="Nama Lengkap" {...register("user_name")} />
@@ -86,7 +97,7 @@ const Register = () => {
 			</form>
 
 			<div className='d-grid gap-0'>
-				<button className={"btn btn-dark mt-4 " +style.btn_signin} type="submit">Buat Akun</button>
+				<button className={"btn btn-dark mt-4 " +style.btn_signin} type="submit" form="register-form">Buat Akun</button>
 			</div>
 			<p className='mt-4 d-flex justify-content-center'>Sudah punya akun 
 			<Link to="/login" className={style.login}>Masuk disini</Link></p>
