0888421ac7cb46d76ed67bc7db2f6ebf78a2da0f
Rizal Lazuardi Firdaus <93332704+rzlzrdf@users.noreply.github.com>
2022-07-24 22:45:49 +0700

    card-profile fixing

--- src/Components/CardProfile/CardProfile.js
+++ src/Components/CardProfile/CardProfile.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import Loading from "../Loading/Loading"
+import jwtDecode from 'jwt-decode';
 
 const CardProfile = () => {
   
@@ -12,9 +13,12 @@ const CardProfile = () => {
   const [loading, setLoading] =useState(true)
  
 const { user } = useSelector((state) => state.auth);
+
+  const jwt= jwtDecode(user)
+
   useEffect(() => {
     axios
-      .get('https://secondhandapp.herokuapp.com/api/user/detail-user/11', {
+      .get(`https://secondhandapp.herokuapp.com/api/user/detail-user/${jwt.id}`, {
         headers: {
           Authorization:
             user,
@@ -32,7 +36,7 @@ const { user } = useSelector((state) => state.auth);
       <Card className={style.card__profile+ " shadow-sm"}>
         <div className={style.profile}>
           <div className={style.left}>
-            <img src={products.foto_profil} alt={''} className={style.img_} />
+            <img src={products.foto_profil !== null ? products.foto_profil : 'https://dummyimage.com/400x400/8f548f/ffffff.png&text=User'} alt={''} className={style.img_} />
           </div>
           <div className={style.right}>
             <div className={style.info}>
--- src/Components/HalamanProduk/Profile.js
+++ src/Components/HalamanProduk/Profile.js
@@ -6,8 +6,8 @@ const Profile = (props) => {
   return (
     <div className={`d-flex ${style.Card_Profile} align-items-center`}>
       <div className={style.pict}>
-        { props.produk.user.foto_profil !== null &&
-          <img  src={props.produk.user.foto_profil}  alt=''/>
+        { props.produk.user.foto_profil !== null ?
+          <img  src={props.produk.user.foto_profil} alt=''/> :  <img  src={'https://dummyimage.com/400x400/8f548f/ffffff.png&text=User'} alt='' />
         }
       </div>
       <div className={`${style.profile}`}>
