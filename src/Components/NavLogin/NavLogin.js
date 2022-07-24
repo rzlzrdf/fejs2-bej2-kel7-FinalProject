dba5da80edcd7c4451da9572a40230bd43999612
Brian Adiyatma <brian.adiyatma.151200@gmail.com>
2022-07-24 12:57:33 +0700

    Notification dan Search

--- src/Components/NavLogin/NavLogin.js
+++ src/Components/NavLogin/NavLogin.js
@@ -1,64 +1,97 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 import { useSelector } from "react-redux";
-import style from './NavLogin.module.css'
-import {FaBell, FaListUl , FaUser, FaHome} from 'react-icons/fa'
-import { Dropdown, Nav, Navbar } from 'react-bootstrap'
-import AppDropdownList from '../AppDropdownList/AppDropdownList'
+import style from "./NavLogin.module.css";
+import { FaBell, FaListUl, FaUser, FaHome } from "react-icons/fa";
+import { Dropdown, Nav, Navbar } from "react-bootstrap";
+import AppDropdownList from "../AppDropdownList/AppDropdownList";
 
 const NavLogin = () => {
-
   const { user } = useSelector((state) => state.auth);
-   const [data, setData] = useState('')
+  const [data, setData] = useState([]);
 
-   useEffect(() => {
-     axios.get('https://secondhandapp.herokuapp.com/api/notif/unread', {
-      headers: {
-        Authorization:
-          user,
-      },
-    })
-      .then((res) => {
-         console.log(res.data.notif)
-         setData(res.data.data)
+  useEffect(() => {
+    axios
+      .get("https://secondhandapp.herokuapp.com/api/notif/unread", {
+        headers: {
+          Authorization: user,
+        },
       })
-   
-     
-   }, [])
+      .then((res) => {
+        console.log(res.data.notif);
+        setData(res.data.notif);
+      });
+  }, []);
 
   return (
     <>
       <Navbar.Toggle className={style.togle} />
-          <Navbar.Collapse className={style.mobile}>
-          <Nav className={"ms-auto "+ style.btn_wrapper}>
-            <Link to='/' id="RouterNavLink" className={'d-md-none ' + style.btn_}><FaHome /> <span> Home</span></Link>
+      <Navbar.Collapse className={style.mobile}>
+        <Nav className={"ms-auto " + style.btn_wrapper}>
+          <Link to="/" id="RouterNavLink" className={"d-md-none " + style.btn_}>
+            <FaHome /> <span> Home</span>
+          </Link>
 
-            <Link id='RouterNavLink' className={style.btn_} to='/daftar-jual'><FaListUl /> <span className='d-md-none '> Daftar Jual</span></Link>
+          <Link id="RouterNavLink" className={style.btn_} to="/daftar-jual">
+            <FaListUl /> <span className="d-md-none "> Daftar Jual</span>
+          </Link>
 
-            <Link to='/notifikasi' id="RouterNavLink" className={'d-md-none ' + style.btn_}><FaBell /> <span> Notifikasi</span></Link>
+          <Link
+            to="/notifikasi"
+            id="RouterNavLink"
+            className={"d-md-none " + style.btn_}
+          >
+            <FaBell /> <span> Notifikasi</span>
+          </Link>
 
-            <Dropdown to="#" align={'end'} className={style.btn_dropdown} >
-              <Dropdown.Toggle variant='white' className={'' + style.icons_menu} id='dropdown-basic' >
-                <FaBell />
-                {/* <span className={"badge rounded-pill badge-notification bg-danger "}></span> */}
-              </Dropdown.Toggle>
-              <Dropdown.Menu className={`p-3 w-auto ${style.dropdownMenu}`}>
-              {/* {data?.map((data, id) => {
-               return(
-               <AppDropdownList img={'./Img/xperia.jpg'} title={'Penawaran produk'} date={'20 Jan 22'} name={'Xperia 1 mark iii'} price={'5.250.000'} desc={'Ditawar Rp 4.200.000'} link={'#/action-1'} />
-                  
-               )
-            })} */}
-            <AppDropdownList/>
-              </Dropdown.Menu>
-            </Dropdown>
+          <Dropdown to="#" align={"end"} className={style.btn_dropdown}>
+            <Dropdown.Toggle
+              variant="white"
+              className={"" + style.icons_menu}
+              id="dropdown-basic"
+            >
+              <FaBell />
+              {/* <span
+                className={"badge rounded-pill badge-notification bg-danger "}
+              ></span> */}
+            </Dropdown.Toggle>
+            <Dropdown.Menu className={`p-3 w-auto ${style.dropdownMenu}`}>
+              {/* <AppDropdownList
+                img={"./Img/xperia.jpg"}
+                title={"Penawaran produk"}
+                date={"20 Jan 22"}
+                name={"Xperia 1 mark iii"}
+                price={"22"}
+                desc={"Ditawar Rp 4.200.000"}
+                link={"#/action-1"}
+              /> */}
+              {data.map((item, i) => {
+                return (
+                  <AppDropdownList
+                    key={i}
+                    img={item.foto_produk_1}
+                    title={item.status_notif.toUpperCase()}
+                    date={"20 Jan 22"}
+                    name={"Xperia 1 mark iii"}
+                    price={new Intl.NumberFormat("id-ID", {
+                      currency: "IDR",
+                    }).format(item.harga)}
+                    // desc={"Ditawar Rp 4.200.000"}
+                    link={"#/action-1"}
+                  />
+                );
+              })}
+            </Dropdown.Menu>
+          </Dropdown>
 
-            <Link to='/info-profil' id="RouterNavLink" className={style.btn_}><FaUser /> <span className='d-md-none'> Akun Saya</span></Link>
-          </Nav>
-        </Navbar.Collapse>
+          <Link to="/info-profil" id="RouterNavLink" className={style.btn_}>
+            <FaUser /> <span className="d-md-none"> Akun Saya</span>
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
     </>
-  )
-}
+  );
+};
 
-export default NavLogin
+export default NavLogin;
--- src/Components/NavbarSearch/NavbarSearch.js
+++ src/Components/NavbarSearch/NavbarSearch.js
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from "react";
 import style from "./NavbarSearch.module.css";
 import { Container, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import NavLogin from "../NavLogin/NavLogin";
 import LoginButton from "../NavbarSearch/LoginButton";
 import jwtDecode from "jwt-decode";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const NavbarSearch = () => {
   const { user } = useSelector((state) => state.auth);
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+  const goToSearch = (target) => {
+    navigate(`/all/${target}`);
+  };
 
   return (
     <Navbar
@@ -29,7 +35,10 @@ const NavbarSearch = () => {
             type={"search"}
             placeholder="Cari di sini ..."
             className={style.search}
+            onSubmit={(e) => console.log("daskjdaskj")}
+            onChange={(e) => setInput(e.target.value)}
           />
+          <button onClick={() => goToSearch(input)}>Search</button>
         </Navbar.Brand>
         {user == null && <LoginButton />}
         {user !== null && <NavLogin />}
